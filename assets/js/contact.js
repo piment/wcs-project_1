1c2d1ea9b1b0218932693e5a9fc2dc3bedc1afde
XAVIERYRIBARREN <xavieryribarren@macbook-pro.home>
2022-03-17 18:52:09 +0100

    Form + CSS sheet + Js for slider

---
+++ assets/js/contact.js
@@ -0,0 +1,20 @@
+let slider = document.getElementById("slider");
+let output = document.getElementById("ageslider");
+output.innerHTML = "";
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  if(slider.value < 30){
+    output.innerHTML = "Ça va";
+  }else if (slider.value < 40){
+    output.innerHTML = "Premières rides";
+  }else if (slider.value < 50){
+  output.innerHTML = "Nivea";
+  }else if (slider.value < 60){
+  output.innerHTML = "Un peu plus de Nivea";
+  }else if (slider.value < 80){
+  output.innerHTML = "Et le reste";
+  }else if (slider.value > 80){
+  output.innerHTML = "Accordéon";
+}
+} 
