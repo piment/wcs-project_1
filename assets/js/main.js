23e436a56310e50ed3ad8c2b5b7233193220fd91
piment <romain@mura.io>
2022-03-18 00:45:43 +0100

    Add temporary testing date

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ const getCounter = () => {
 
   //Get the date of the next session
   const nextSessionDate = new Date(new Date().setDate(toDay.getDate() + daysToNextSession));
-  
+  document.querySelector(".nextSessionDate").innerHTML = nextSessionDate.toLocaleDateString();
   //Get the number of hour, minutes and seconds (minus the number of days) to the next session
   const newHour = 24 - (toDay.getHours() === 0 ? 24 : toDay.getHours());
   const newMinute = 60 - (toDay.getMinutes() === 0 ? 60 : toDay.getMinutes()) - 1;
@@ -21,8 +21,9 @@ const getCounter = () => {
 }
 
 document.querySelector(".setDate").addEventListener("change", (ev) => {
-  console.log(ev.currentTarget.value);
-  startDay = new Date(ev.currentTarget.value);
+  const date = ev.currentTarget.value.split("-");
+  console.log(`${date[1]}/${date[2]}/${date[0]}`);
+  startDay = new Date(`${date[1]}/${date[2]}/${date[0]}`);
 })
 
 setInterval(getCounter, 1000)
