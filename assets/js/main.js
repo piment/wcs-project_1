a7b55093bd8d530c6f57d0229b3ed443b848e04b
Romain MURA <romain@mura.io>
2022-03-21 23:55:16 +0100

    refactoring counter function in main.js and changing display of counter in index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,31 @@
-const startDay = new Date("03/01/2022");
+const dayOfFirstSession = new Date('01/01/2022');
 const daysBetweenSession = 28;
 
-const getCounter = (startDay, daysBetweenSession) => {
+const getCounter = (startDay, sessionInterval) => {
   const toDay = new Date();
 
   // Get the number of days to the next session
-  const daysToNextSession =
-    Math.floor(startDay.getTime() / (24 * 60 * 60 * 1000)) % daysBetweenSession;
+  const daysToNextSession = 28 - (Math.ceil((toDay - startDay) / (1000 * 60 * 60 * 24)) % sessionInterval);
 
   // Get the date of the next session
-  const nextSessionDate = new Date(
-    new Date().setDate(toDay.getDate() + daysToNextSession)
-  );
-  document.querySelector(".nextSessionDate").innerHTML =
-    nextSessionDate.toLocaleDateString();
+  const nextSessionDate = new Date(new Date().setDate(toDay.getDate() + daysToNextSession));
+
+  document.querySelector('.nextSessionDate').textContent = `(${nextSessionDate.toLocaleDateString()})`;
+
   // Get the number of hour, minutes and seconds (minus the number of days) to the next session
-  const newHour = 24 - (toDay.getHours() === 0 ? 24 : toDay.getHours());
-  const newMinute =
-    60 - (toDay.getMinutes() === 0 ? 60 : toDay.getMinutes()) - 1;
+  const newHour = 24 - (toDay.getHours() === 0 ? 24 : toDay.getHours()) - 1;
+  const newMinute = 60 - (toDay.getMinutes() === 0 ? 60 : toDay.getMinutes()) - 1;
   const newSecond = 60 - (toDay.getSeconds() === 0 ? 60 : toDay.getSeconds());
 
-  const counter = document.querySelector(".counterTime");
+  const day = document.querySelector('.day');
+  const hour = document.querySelector('.hour');
+  const minute = document.querySelector('.minute');
+  const second = document.querySelector('.second');
 
-  counter.innerHTML = `${daysToNextSession
-    .toString()
-    .padStart(2, "0")}:${newHour.toString().padStart(2, "0")}:${newMinute
-    .toString()
-    .padStart(2, "0")}:${newSecond.toString().padStart(2, "0")}`;
+  day.textContent = `${daysToNextSession.toString().padStart(2, '0')}`;
+  hour.textContent = `${newHour.toString().padStart(2, '0')}`;
+  minute.textContent = `${newMinute.toString().padStart(2, '0')}`;
+  second.textContent = `${newSecond.toString().padStart(2, '0')}`;
 };
 
-setInterval(getCounter, 1000, startDay, daysBetweenSession);
-
-const getNextDates = (startDate, interval, nbOfDates) => {
-  const dates = [].fill(nbOfDates, 0);
-};
-console.log(getNextDates(startDate, daysBetweenSessions, 3));
+setInterval(getCounter, 1000, dayOfFirstSession, daysBetweenSession);
